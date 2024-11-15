168f21ff35ddced6d8b81fea99ec5d438d37a30b
Egba Frank <Egbafrank72@gmail.com>
2024-11-15 00:01:45 +0000

    add args to base model initiator

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-"""UserSession module
+"""User Session module
 """
-from .base import Base
+from models.base import Base
 
 
 class UserSession(Base):
-    """UserSession definitions"""
+    """User Session definitions"""
 
     def __init__(self, *args: list, **kwargs: dict):
         """Initialize a UserSession instance"""
-        super().__init__()
+        super().__init__(*args, **kwargs)
         self.user_id = kwargs.get("user_id")
         self.session_id = kwargs.get("session_id")
