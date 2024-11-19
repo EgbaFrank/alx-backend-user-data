c7e6b35423786435d059385e32e1c630aeb30957
Egba Frank <Egbafrank72@gmail.com>
2024-11-19 22:51:27 +0000

    Improve password update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -108,9 +108,17 @@ class Auth:
             raise ValueError
         try:
             user = self._db.find_user_by(reset_token=reset_token)
+
+            if not user:
+                raise ValueError
+
             hashed_password = _hash_password(password)
-            self._db.update_user(user.id, hashed_password=hashed_password)
-            self._db.update_user(user.id, reset_token=None)
+
+            self._db.update_user(
+                user.id,
+                hashed_password=hashed_password,
+                reset_token=None
+            )
 
             return None
 
